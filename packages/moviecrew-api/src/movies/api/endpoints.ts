74d8c195782526f254559e378d76dacdfbc2c73c
Maxime Mohandi <mohandi.maxime@gmail.com>
2023-11-18 15:04:26 +0100

    fix(moviecrew-api): 🐛 set the correct endpoint for fetching all movies

--- packages/moviecrew-api/src/movies/api/endpoints.ts
+++ packages/moviecrew-api/src/movies/api/endpoints.ts
@@ -1,7 +1,7 @@
 import config from "../../config";
 
 const MOVIE_API_BASE_URL = config.baseUrl;
-export const GET_MOVIES_ENDPOINT = `${MOVIE_API_BASE_URL}/movies`;
+export const GET_MOVIES_ENDPOINT = `${MOVIE_API_BASE_URL}/movie/all`;
 export const GET_MOVIE_ENDPOINT = `${MOVIE_API_BASE_URL}/movie/details`;
 export const GET_RANDOM_MOVIE_ENDPOINT = `${MOVIE_API_BASE_URL}/movie/random`;
 export const POST_MOVIE_ENDPOINT = `${MOVIE_API_BASE_URL}/movie/add`;
