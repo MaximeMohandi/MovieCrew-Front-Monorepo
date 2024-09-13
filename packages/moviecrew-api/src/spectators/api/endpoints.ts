db5f9bce25d26d05597009ef407f9202c634d344
Maxime Mohandi <mohandi.maxime@gmail.com>
2024-09-13 18:32:05 +0200

    fix(moviecrew-api): 🐛 wrong endpoint for spectator

--- packages/moviecrew-api/src/spectators/api/endpoints.ts
+++ packages/moviecrew-api/src/spectators/api/endpoints.ts
@@ -1,5 +1,5 @@
 import config from "../../config";
 
-const SPECTATOR_API_BASE_URL = `${config.baseUrl}/spectators`;
+const SPECTATOR_API_BASE_URL = `${config.baseUrl}/spectator`;
 export const GET_SPECTATORS_DETAILS = (spectatorId: string) =>
   `${SPECTATOR_API_BASE_URL}/${spectatorId}/details`;
