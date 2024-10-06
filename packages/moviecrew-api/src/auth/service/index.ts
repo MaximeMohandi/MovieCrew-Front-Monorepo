cfb8c941290042d2e1b80f42aa32f261df32f7db
Maxime Mohandi <mohandi.maxime@gmail.com>
2024-10-06 12:18:07 +0200

    feat(moviecrew-api): 🎸 add a method to re-auth the app

--- packages/moviecrew-api/src/auth/service/index.ts
+++ packages/moviecrew-api/src/auth/service/index.ts
@@ -1,14 +1,32 @@
 import axios from "axios";
 import { getToken } from "../api";
 
+const authData: {
+  clientId?: number;
+  secret?: string;
+} = {};
+
+const authenticateAppFromAuthData = async () => {
+  const { token, expirationDate } = await getToken(
+    authData.clientId!,
+    authData.secret!,
+  );
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+
+  return expirationDate;
+};
+
 export const authenticateApp = async (
   apiUrl: string,
   clientId: number,
   secret: string,
 ): Promise<Date> => {
+  authData.clientId = clientId;
+  authData.secret = secret;
   axios.defaults.baseURL = apiUrl;
-  const { token, expirationDate } = await getToken(clientId, secret);
-  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 
-  return expirationDate;
+  return await authenticateAppFromAuthData();
 };
+
+export const reAuthenticateApp = async (): Promise<Date> =>
+  await authenticateAppFromAuthData();
--- packages/moviecrew-api/test/auth/service/authenticateApp.test.ts
+++ packages/moviecrew-api/test/auth/service/authenticateApp.test.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as authApi from "../../../src/auth/api";
-import { authenticateApp } from "../../../src/auth/service";
+import { authenticateApp, reAuthenticateApp } from "../../../src/auth/service";
 
 describe("when authenticating an app", () => {
   it("authenticate to the api and set the default token and URL then return expirationDate", async () => {
@@ -23,5 +23,29 @@ describe("when authenticating an app", () => {
     );
     expect(axios.defaults.baseURL).toEqual("https://fake-api.com");
     expect(expirationDate.toDateString()).toEqual(new Date().toDateString());
+  });
+
+  it("should get new token when called with refresh", async () => {
+    // Arrange
+    let mockExpirationDate = new Date(Date.now());
+    jest.spyOn(authApi, "getToken").mockImplementation(() => {
+      mockExpirationDate = new Date(mockExpirationDate.getTime() + 10000);
+      return Promise.resolve({
+        token: "this.a.token",
+        expirationDate: mockExpirationDate,
+      });
+    });
+
+    // Act
+    const expirationDate = await authenticateApp(
+      "https://fake-api.com",
+      1,
+      "secret",
+    );
+
+    const newExpirationDate = await reAuthenticateApp();
+
+    // Assert
+    expect(expirationDate.getTime()).toBeLessThan(newExpirationDate.getTime());
   });
 });
